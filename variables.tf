0ba2d40457f84f53f74ca24463fb2141d766fa78
Rickard Löfström <rickard.lofstrom@gmail.com>
2018-08-07 15:23:34 +0200

    fix: removed optionals

--- variables.tf
+++ variables.tf
@@ -82,7 +82,7 @@ variable "health_check" {
 
 variable "health_check_grace_period_seconds" {
   default     = "300"
-  description = " (Optional) Seconds to ignore failing load balancer health checks on newly instantiated tasks to prevent premature shutdown, up to 7200. Only valid for services configured to use load balancers."
+  description = "Seconds to ignore failing load balancer health checks on newly instantiated tasks to prevent premature shutdown, up to 7200. Only valid for services configured to use load balancers."
 }
 
 variable "tags" {
