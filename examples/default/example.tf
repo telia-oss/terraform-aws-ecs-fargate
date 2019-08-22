217eea4d5829d0a93d9df7f50aa335bce85f4f7b
Colin Coleman <colin.coleman@kontrollsmart.com>
2019-08-22 13:18:49 +0200

    remove quotes from port numbers in example - terraform is now much better with types

--- examples/default/example.tf
+++ examples/default/example.tf
@@ -36,7 +36,7 @@ module "fargate_alb" {
 
 resource "aws_lb_listener" "alb" {
   load_balancer_arn = module.fargate_alb.arn
-  port              = "80"
+  port              = 80
   protocol          = "HTTP"
 
   default_action {
@@ -49,8 +49,8 @@ resource "aws_security_group_rule" "task_ingress_8000" {
   security_group_id        = module.fargate.service_sg_id
   type                     = "ingress"
   protocol                 = "tcp"
-  from_port                = "8000"
-  to_port                  = "8000"
+  from_port                = 8000
+  to_port                  = 8000
   source_security_group_id = module.fargate_alb.security_group_id
 }
 
@@ -58,8 +58,8 @@ resource "aws_security_group_rule" "alb_ingress_80" {
   security_group_id = module.fargate_alb.security_group_id
   type              = "ingress"
   protocol          = "tcp"
-  from_port         = "80"
-  to_port           = "80"
+  from_port         = 80
+  to_port           = 80
   cidr_blocks       = ["0.0.0.0/0"]
   ipv6_cidr_blocks  = ["::/0"]
 }
