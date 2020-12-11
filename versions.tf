3856e107a459e1306ca641cdf5446e531e244a42
Lars Tobias Skjong-Børsting <larstobi@relatime.no>
2020-12-11 12:51:50 +0100

    Move provider into required_providers

--- versions.tf
+++ versions.tf
@@ -1,5 +1,9 @@
 terraform {
   required_version = ">= 0.12"
-}
 
-provider "aws" { version = ">= 2.0.0, < 3.0.0" }
+  required_providers {
+    aws = {
+      version = ">= 2.0.0, < 3.0.0"
+    }
+  }
+}
