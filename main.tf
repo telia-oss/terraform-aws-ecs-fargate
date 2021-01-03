85dddc6aa1391a47a0937f1cfd9caadb7e522fba
Jonathan Mickle <jon@thentwrk.com>
2021-01-03 15:36:43 -0800

    can we trick it with a null resource?

--- main.tf
+++ main.tf
@@ -151,7 +151,7 @@ resource "aws_ecs_task_definition" "task" {
     name = (var.create_efs_vol == true ? "${var.name_prefix}-service-storage" : "")
 
     efs_volume_configuration {
-      file_system_id = (var.create_efs_vol == true ? aws_efs_file_system.fs[0].id : "")
+      file_system_id = (var.create_efs_vol == true ? aws_efs_file_system.fs[0].id : null_resource.fs.id)
       root_directory = (var.create_efs_vol == true ? "/opt/data" : "" )
     }
   }
@@ -240,3 +240,6 @@ resource "aws_ecs_service" "service" {
 resource "null_resource" "lb_exists" {
   triggers = var.lb_arn == "" ? {} : { alb_name = var.lb_arn }
 }
+
+
+resource "null_resource" "fs" {}
