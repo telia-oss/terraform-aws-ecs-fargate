ff3814adb4b3cc5cb4bc3fbff6d9efbd40a4d1f4
Jonathan Mickle <jon@thentwrk.com>
2021-01-03 17:50:56 -0800

    typo

--- main.tf
+++ main.tf
@@ -156,7 +156,7 @@ resource "aws_security_group" "efs_service" {
   )
 }
 
-resource "aws_security_group_rule" "egress_service" {
+resource "aws_security_group_rule" "efs_egress_service" {
   security_group_id = aws_security_group.efs_service.id
   type              = "egress"
   protocol          = "-1"
@@ -166,7 +166,7 @@ resource "aws_security_group_rule" "egress_service" {
   ipv6_cidr_blocks  = ["::/0"]
 }
 
-resource "aws_security_group_rule" "ingress_service" {
+resource "aws_security_group_rule" "efs_ingress_service" {
   security_group_id = aws_security_group.efs_service.id
   type              = "ingress"
   protocol          = "-1"
