e8cc87cd07074516694df903f35bf63d16f61e07
Cyril Feraudet <cyril@feraudet.com>
2021-05-25 14:25:30 +0200

    Add task_definition ignore_changes

--- main.tf
+++ main.tf
@@ -222,6 +222,9 @@ resource "aws_ecs_service" "service" {
       container_name = var.container_name != "" ? var.container_name : var.name_prefix
     }
   }
+  lifecycle {
+    ignore_changes = [task_definition]
+  }
 }
 
 # HACK: The workaround used in ecs/service does not work for some reason in this module, this fixes the following error:
